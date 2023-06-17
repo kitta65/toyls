3ba7fa2ebdd35221a0da8cee741afd70223671ae
dr666m1 <skndr666m1@gmail.com>
2023-06-17 23:11:31 +0900

    log setting

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"log" // NOTE try slog when v1.21 is released
+	"os"
 )
 
 func main() {
-	fmt.Println("hello world!")
+	// configure log
+	f, err := os.Create("/tmp/toyls.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close() // this may return error so it is a little optimistic
+	log.SetOutput(f)
+
+	// initialize server
+	log.Println("initializing server...")
 }
